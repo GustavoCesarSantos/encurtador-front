3a23d24b1b86551113690b1e510328344fa0760c
GustavoCesarSantos <gustavocs789@gmail.com>
2024-10-26 15:26:54 -0300

    style: formata código

--- src/shared/colors.ts
+++ src/shared/colors.ts
@@ -1,10 +1,9 @@
 export const colors = {
-  primary: '#1E90FF',
-  background: '#F5F5F5',
-  textPrimary: '#333333',
-  inputBackground: '#FFFFFF',
-  inputBorder: '#B0B0B0',
-  placeholderText: '#666666',
-  buttonText: '#FFFFFF',
+    primary: '#1E90FF',
+    background: '#F5F5F5',
+    textPrimary: '#333333',
+    inputBackground: '#FFFFFF',
+    inputBorder: '#B0B0B0',
+    placeholderText: '#666666',
+    buttonText: '#FFFFFF',
 };
-
