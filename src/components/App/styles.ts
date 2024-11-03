60056bd4fa4cf40e980a75f08514f0aa207f6c4a
GustavoCesarSantos <gustavocs789@gmail.com>
2024-11-02 21:09:56 -0300

    style: formata código

--- src/components/App/styles.ts
+++ src/components/App/styles.ts
@@ -16,7 +16,7 @@ export const ContentContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-`
+`;
 
 export const MainTitle = styled.h1`
     font-size: 7rem;
