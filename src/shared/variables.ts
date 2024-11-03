e10e8e149d1cab04d4d3ecc97fb33ea52b82e26d
GustavoCesarSantos <gustavocs789@gmail.com>
2024-11-03 05:48:48 -0300

    fix: corrige variaveis de ambiente

--- src/shared/variables.ts
+++ src/shared/variables.ts
@@ -1,6 +1,6 @@
 export const variables = {
     apiUrl: import.meta.env.VITE_BASE_URL,
-    domainUrl: import.meta.env.DOMAIN_URL ?? 'http://127.0.0.1:5173',
-    accessToken: import.meta.env.ACCESS_TOKEN ?? 'accessToken',
-    refreshToken: import.meta.env.REFRESH_TOKEN ?? 'refreshToken',
+    domainUrl: import.meta.env.VITE_DOMAIN_URL ?? 'http://127.0.0.1:5173',
+    accessToken: import.meta.env.VITE_ACCESS_TOKEN ?? 'accessToken',
+    refreshToken: import.meta.env.VITE_REFRESH_TOKEN ?? 'refreshToken',
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,8 +2,8 @@
 interface ImportMeta {
     env: {
         VITE_BASE_URL: string;
-        DOMAIN_URL: string;
-        ACCESS_TOKEN: string;
-        REFRESH_TOKEN: string;
+        VITE_DOMAIN_URL: string;
+        VITE_ACCESS_TOKEN: string;
+        VITE_REFRESH_TOKEN: string;
     };
 }
