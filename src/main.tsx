3497b2777acc30b3396c31c623e0b996bda8b4ab
GustavoCesarSantos <gustavocs789@gmail.com>
2024-11-03 06:15:02 -0300

    chore: teste de rota

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />} />
-            <Route path=":code" element={<Redirect />} />
+            <Route path="/:code" element={<Redirect />} />
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     </BrowserRouter>,
