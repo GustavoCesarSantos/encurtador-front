4a554a12ef8af483e1aa94d42e258e791e2510ae
GustavoCesarSantos <gustavocs789@gmail.com>
2024-11-03 03:04:34 -0300

    refactor: refatora projeto

--- src/shared/variables.ts
+++ src/shared/variables.ts
@@ -1,6 +1,6 @@
 export const variables = {
     apiUrl: import.meta.env.VITE_BASE_URL,
-    domainUrl: 'http://127.0.0.1:5173',
-    accessToken: 'accessToken',
-    refreshToken: 'refreshToken',
+    domainUrl: import.meta.env.DOMAIN_URL ?? 'http://127.0.0.1:5173',
+    accessToken: import.meta.env.ACCESS_TOKEN ?? 'accessToken',
+    refreshToken: import.meta.env.REFRESH_TOKEN ?? 'refreshToken',
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,9 @@
 // / <reference types="vite/client" />
 interface ImportMeta {
-    env: { VITE_BASE_URL: string };
+    env: {
+        VITE_BASE_URL: string;
+        DOMAIN_URL: string;
+        ACCESS_TOKEN: string;
+        REFRESH_TOKEN: string;
+    };
 }
